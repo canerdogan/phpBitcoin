b8adf60fce192fc7eef45f398907e5ee449cfc5e
jybox <m@jybox.net>
2013-11-20 13:33:30 +0800

    example

--- example.php
+++ example.php
@@ -1,7 +1,40 @@
 <?php
 
-$callback = function() {
+$callback = function($orderID, $inputAddress, $btc, $confirmations, $inputTxHash, $param) {
+    $order = DB::select("order", ["id" => $orderID]);
+    if($order["status"] == "wait_payment")
+    {
+        if($confirmations >= 3)
+        {
+            if($btc == doubleval($order["price"]))
+            {
+                DB::update("order", ["id" => $orderID], ["status" => "finish"]);
+                return true;
+            }
+            else
+            {
+                DB::update("order", ["id" => $orderID], ["status" => "error"]);
+                return true;
+            }
+        }
 
+        DB::update("order", ["id" => $orderID], ["status" => "error"]);
+        return true;
+    }
+
+    return false;
 };
 
 $btc = new Bitcoin("http://127.0.0.1/", "13v2BTCMZMHg5v87susgg86HFZqXERuwUd", "0987654321", $callback);
+
+// genAddress
+
+$orderID = DB::insert("order", ["status" => "wait_genAddress", "price" => "0.1"]);
+$address = $btc->genAddress($orderID);
+DB::update("order", ["id" => $orderID], ["address" => $address, "status" => "wait_payment"]);
+
+echo "Please pay to {$address}";
+
+// callback
+
+echo $btc->doCallback();
