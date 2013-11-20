3539aba27bd2737614a97d4f0b675d0b5923434f
jybox <m@jybox.net>
2013-11-20 13:36:42 +0800

    update example

--- example.php
+++ example.php
@@ -2,27 +2,20 @@
 
 $callback = function($orderID, $inputAddress, $btc, $confirmations, $inputTxHash, $param) {
     $order = DB::select("order", ["id" => $orderID]);
+
+    if($confirmations < 3)
+        return false;
+
     if($order["status"] == "wait_payment")
     {
-        if($confirmations >= 3)
-        {
-            if($btc == doubleval($order["price"]))
-            {
-                DB::update("order", ["id" => $orderID], ["status" => "finish"]);
-                return true;
-            }
-            else
-            {
-                DB::update("order", ["id" => $orderID], ["status" => "error"]);
-                return true;
-            }
-        }
-
-        DB::update("order", ["id" => $orderID], ["status" => "error"]);
-        return true;
+        if($btc == doubleval($order["price"]))
+            DB::update("order", ["id" => $orderID], ["status" => "finish"]);
+        else
+            DB::update("order", ["id" => $orderID], ["status" => "error"]);
+            return true;
     }
 
-    return false;
+    return true;
 };
 
 $btc = new Bitcoin("http://127.0.0.1/", "13v2BTCMZMHg5v87susgg86HFZqXERuwUd", "0987654321", $callback);
